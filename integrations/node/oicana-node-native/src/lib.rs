b263916d56347c795ba3429ede11e546537e3305
Niklas Eicker <git@nikl.me>
2025-10-05 17:09:18 +0200

    Add @oicana/node

--- integrations/node/oicana-node-native/src/lib.rs
+++ integrations/node/oicana-node-native/src/lib.rs
@@ -9,6 +9,9 @@ extern crate napi_derive;
 
 use napi::bindgen_prelude::{Buffer, Result, Uint8Array};
 use napi::Error;
+use oicana_export::pdf::export_merged_pdf;
+use oicana_export::png::export_merged_png;
+use oicana_export::svg::export_merged_svg;
 use oicana_files::packed::PackedTemplate;
 use oicana_files::TemplateFiles;
 use oicana_input::input::blob::{Blob, BlobInput};
@@ -16,14 +19,15 @@ use oicana_input::input::json::JsonInput;
 use oicana_input::{CompilationConfig, TemplateInputs};
 use oicana_world::manifest::OicanaWorldFiles;
 use oicana_world::world::OicanaWorld;
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::io::Cursor;
+use std::str::FromStr;
 use std::sync::Mutex;
-use typst::foundations::{Bytes, Smart, Value};
-use typst::layout::{Abs, PagedDocument};
+use typst::foundations::Bytes;
+use typst::layout::PagedDocument;
 use typst::syntax::{FileId, VirtualPath};
 use typst::utils::once_cell::sync::OnceCell;
-use typst_pdf::{PdfOptions, PdfStandard, PdfStandards};
 
 /// Error string when a requested template is not registered yet. Call `[register_template]` before
 /// trying to use the template through a different method.
@@ -39,24 +43,28 @@ pub fn register_template(
   files: Uint8Array,
   json_inputs: HashMap<String, String>,
   blob_inputs: HashMap<String, BlobWithMetadata>,
-  export_format: ExportFormat,
+  export_format: String,
+  compilation_mode: CompilationMode,
 ) -> Result<Buffer> {
+  let export_format =
+    serde_json::from_str(&export_format).map_err(|error| Error::from_reason(error.to_string()))?;
   let files = PackedTemplate::new(Cursor::new(files));
   let manifest = files
     .manifest()
     .map_err(|error| Error::from_reason(error.to_string()))?;
-  println!("inserting new world for template '{template}'");
 
-  let inputs = prepare_inputs(json_inputs, blob_inputs);
+  let mut inputs = prepare_inputs(json_inputs, blob_inputs);
+  inputs.with_config(compilation_mode.into());
   let mut zip_world = OicanaWorld::new(files, inputs, manifest)
     .map_err(|error| Error::from_reason(error.to_string()))?;
 
   let document = zip_world
     .compile()
     .map_err(|error| Error::from_reason(error.to_string()))?;
-  world().lock().unwrap().insert(template, zip_world);
 
-  export(&document.document, export_format)
+  let result = export(&document.document, &zip_world, export_format);
+  world().lock().unwrap().insert(template, zip_world);
+  result
 }
 
 /// Compile the identified template with the given inputs.
@@ -68,21 +76,24 @@ pub fn compile_template(
   template: String,
   json_inputs: HashMap<String, String>,
   blob_inputs: HashMap<String, BlobWithMetadata>,
-  export_format: ExportFormat,
+  export_format: String,
+  compilation_mode: CompilationMode,
 ) -> Result<Buffer> {
+  let export_format =
+    serde_json::from_str(&export_format).map_err(|error| Error::from_reason(error.to_string()))?;
   let mut cache_lock = world().lock().unwrap();
   let Some(world) = cache_lock.get_mut(&template) else {
     return Err(Error::from_reason("Template was not registered"));
   };
   let mut inputs = prepare_inputs(json_inputs, blob_inputs);
-  inputs.with_config(CompilationConfig::production());
+  inputs.with_config(compilation_mode.into());
   world.update_inputs(inputs);
 
   let document = world
     .compile()
     .map_err(|error| Error::from_reason(error.to_string()))?;
 
-  export(&document.document, export_format)
+  export(&document.document, world, export_format)
 }
 
 /// Load all input definitions for the given template.
@@ -134,20 +145,25 @@ pub fn get_file(template: String, file: String) -> Result<Buffer> {
   Ok(bytes.to_vec().into()) // This is currently copying!
 }
 
-fn export(document: &PagedDocument, export_format: ExportFormat) -> Result<Buffer> {
+fn export(
+  document: &PagedDocument,
+  world: &OicanaWorld<PackedTemplate>,
+  export_format: ExportFormat,
+) -> Result<Buffer> {
   match export_format {
-    ExportFormat::Png => Ok(create_image(document, 1.)?.into()),
-    ExportFormat::Pdf => {
-      let options = PdfOptions {
-        ident: Smart::Auto,
-        timestamp: None,
-        page_ranges: None,
-        standards: PdfStandards::new(&[PdfStandard::A_3b]).expect("Invalid PDF standards"),
-      };
-      let pdf = typst_pdf::pdf(document, &options)
-        .map_err(|error| Error::from_reason(format!("{error:?}")))?;
+    ExportFormat::Png { pixels_per_pt } => {
+      let pix_map_result = export_merged_png(document, pixels_per_pt);
+      pix_map_result
+        .map_err(|error| Error::from_reason(format!("Failed to encode PNG: {error:?}")))
+        .map(|pix_map| pix_map.into())
+    }
+    ExportFormat::Pdf => export_merged_pdf(document, world)
+      .map_err(|error| Error::from_reason(format!("Failed to encode PNG: {error:?}")))
+      .map(|pdf| pdf.into()),
+    ExportFormat::Svg => {
+      let svg = export_merged_svg(document);
 
-      Ok(pdf.into())
+      Ok(svg.into())
     }
   }
 }
@@ -177,33 +193,40 @@ fn add_blob_inputs(
 ) {
   for (key, value) in blob_inputs.drain() {
     let mut blob = Blob::from(Bytes::new(value.bytes.to_vec()));
-    blob.metadata.extend(
-      value
-        .meta
-        .into_iter()
-        .map(|(meta_key, meta_value)| (meta_key.into(), Value::Str(meta_value.into()))),
-    );
+    blob.metadata = Deserialize::deserialize(serde_json::Value::from_str(&value.meta).unwrap())
+      .map_err(|error| format!("Failed to deserialize from JSON value: {error:?}"))
+      .unwrap();
     inputs.with_input(BlobInput::new(key, blob));
   }
 }
 
-fn create_image(document: &PagedDocument, pixels_per_pt: f32) -> Result<Vec<u8>> {
-  typst_render::render_merged(document, pixels_per_pt, Abs::pt(15.), None)
-    .encode_png()
-    .map_err(|error| Error::from_reason(format!("{error:?}")))
+#[derive(Deserialize)]
+#[serde(tag = "format")]
+enum ExportFormat {
+  #[serde(alias = "png")]
+  Png {
+    #[serde(rename = "pixelsPerPt")]
+    pixels_per_pt: f32,
+  },
+  #[serde(alias = "pdf")]
+  Pdf,
+  #[serde(alias = "svg")]
+  Svg,
 }
 
-/// The supported export formats of a template.
 #[napi]
-pub enum ExportFormat {
-  /// Render the template into a png image.
-  ///
-  /// The image is not optimized for file size to speed up compiling.
-  Png,
-  /// Render the template to a PDF file.
-  ///
-  /// The currently exported standard is PDF/A-3b
-  Pdf,
+pub enum CompilationMode {
+  Production,
+  Development,
+}
+
+impl From<CompilationMode> for oicana_input::CompilationConfig {
+  fn from(value: CompilationMode) -> Self {
+    match value {
+      CompilationMode::Development => CompilationConfig::development(),
+      CompilationMode::Production => CompilationConfig::production(),
+    }
+  }
 }
 
 /// A blob with its metadata.
@@ -212,7 +235,7 @@ pub struct BlobWithMetadata {
   /// The byte content of the blob.
   pub bytes: Uint8Array,
   /// Metadata of the blob.
-  pub meta: HashMap<String, String>,
+  pub meta: String,
 }
 
 fn world() -> &'static Mutex<HashMap<String, OicanaWorld<PackedTemplate>>> {
---
+++ integrations/node/oicana-node/README.md
@@ -0,0 +1,9 @@
+# Oicana
+*Dynamic PDF Generation based on Typst*
+
+https://oicana.com
+
+Oicana offers seamless PDF templating across multiple platforms. Define your templates in Typst, specify dynamic inputs, and generate high quality PDFs from any environment - whether it's a web browser, server application, or desktop software.
+
+> **Oicana is in Alpha! It is rough around the edges and has a limited number of integrations.**
+
