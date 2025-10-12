5e7a931c2d9af82df804b9d29e3492626a754d14
Niklas Eicker <git@nikl.me>
2025-10-12 22:00:44 +0200

    Update and format node integration build

--- integrations/node/oicana-node-native/index.d.ts
+++ integrations/node/oicana-node-native/index.d.ts
@@ -10,7 +10,7 @@ export interface BlobWithMetadata {
 
 export declare const enum CompilationMode {
   Production = 0,
-  Development = 1
+  Development = 1,
 }
 
 /**
@@ -19,7 +19,6 @@ export declare const enum CompilationMode {
  * Calling this method requires a previous call to [`register_template`] with the same template
  * identifier.
  */
-export declare function compileTemplate(template: string, jsonInputs: Record<string, string>, blobInputs: Record<string, BlobWithMetadata>, exportFormat: string, compilationMode: CompilationMode): Buffer
 export declare function compileTemplate(
   template: string,
   jsonInputs: Record<string, string>,
@@ -69,7 +68,6 @@ export const NOT_REGISTERED: string
  * compile it once with the given inputs. The Typst [`typst::World`] will be cached and reused for
  * subsequent calls to the other methods with the same template identifier.
  */
-export declare function registerTemplate(template: string, files: Uint8Array, jsonInputs: Record<string, string>, blobInputs: Record<string, BlobWithMetadata>, exportFormat: string, compilationMode: CompilationMode): Buffer
 export declare function registerTemplate(
   template: string,
   files: Uint8Array,
