b6f8980693a287b9380904c5452c727e5ac1aee9
Niklas Eicker <git@nikl.me>
2026-09-19 19:49:02 +0200

    Bump version to 0.9.0-rc.3

--- integrations/java/build.gradle.kts
+++ integrations/java/build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 
 subprojects {
     group = "com.oicana"
-    version = "0.9.0-rc.2"
+    version = "0.9.0-rc.3"
 
     apply(plugin = "java-library")
     apply(plugin = "com.vanniktech.maven.publish")
--- integrations/python/oicana-python/src/oicana/__init__.py
+++ integrations/python/oicana-python/src/oicana/__init__.py
@@ -36,7 +36,7 @@ from .types import (
     ZipLimits,
 )
 
-__version__ = "0.9.0rc2"
+__version__ = "0.9.0rc3"
 
 __all__ = [
     "Template",
